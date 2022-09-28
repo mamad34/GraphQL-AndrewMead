fed0ccb0ff19a3240768d38f7ad537c00fd60540
mamad <mohammadtt77@yahoo.com>
2022-09-28 16:46:57 +0330

    season 3

---
+++ graphql-basics/playground/myModule.js
@@ -0,0 +1,11 @@
+const message = "dick to in zendegi";
+
+const name = "mamad";
+
+const location = "iran";
+
+const getGreeting = (name) => {
+  return `welcome to the course ${name}`;
+};
+
+export { message, name, getGreeting, location as default };
